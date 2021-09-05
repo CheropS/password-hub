086a458f522bf54abc45842d8e848a7d801a85c8
Sharon Cherop <sh.cherop@gmail.com>
2021-09-05 09:52:44 +0300

    Introduced user class and defined the variables

---
+++ password.py
@@ -0,0 +1,13 @@
+class User:
+    """
+    Class that generates a login username and password for existing users
+    """
+
+    def __init__(self, username, password):
+        """
+        init method that helps us define our objects
+
+        Args: 
+            username= new username.
+            password = New password.
+        """
