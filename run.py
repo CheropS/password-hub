5a392f51a35f12f66c0219707abc355c06aed535
Sharon Cherop <sh.cherop@gmail.com>
2021-09-06 06:36:43 +0300

    Adding dunctions for saving, deleting, displaying and creating new credentials

--- run.py
+++ run.py
@@ -1,2 +1,27 @@
 from password import User
 from password import Credentials
+
+def create_user(login_name,pword):
+    """
+    This is a function that creates a new user 
+    """
+    new_credentials=Credentials(login_name,pword)
+    return new_credentials
+
+def save_credentials(credentials):
+    """
+    This is a function to save credentials
+    """
+    credentials.save_credentials()
+
+def del_credentials(credentials):
+    """
+    Function to delete a credential
+    """
+    credentials.delete_credentials()
+
+def display_credentials():
+    """
+    Function that returns a list of saved credentials
+    """
+    return Credentials.display_credentials()
